8e6f53afb84f241a6a9b6d0db569271121f81afe
nikita <svetlichnyi.ns@phystech.edu>
2022-08-09 18:08:59 +0300

    Made a refactoring of the program, drawing Mandelbrot's set; added comments there

--- Mandelbrot_set.cpp
+++ Mandelbrot_set.cpp
@@ -13,11 +13,11 @@ void Mandelbrot_set (VertexArray& vertex_array, int precision, float zoom, Rende
   for (int i = 0; i < window.getSize().y; i++) {
     for (int j = 0; j < window.getSize().x; j++) {
       complex_number c;
-      c.real = (long double) ((j - (int) (window.getSize().x / 2)) / zoom);
+      c.real = (long double) ((j - (int) (window.getSize().x / 2)) / zoom);  // drawing Mandelbrot's set, we move point (0; 0) to the center of the window
       c.imaginary = (long double) ((i - (int) (window.getSize().y / 2)) / zoom);
       complex_number z = c;
       int k;
-      for (k = 1; k <= precision; k++) {
+      for (k = 1; k <= precision; k++) {  // the recurrent sequence: z_(n+1) = (z_n)^2 + c
         complex_number tmp;
         tmp.real = z.real * z.real - z.imaginary * z.imaginary + c.real;
         tmp.imaginary = 2 * z.real * z.imaginary + c.imaginary;
@@ -25,21 +25,15 @@ void Mandelbrot_set (VertexArray& vertex_array, int precision, float zoom, Rende
         if (z.real * z.real + z.imaginary * z.imaginary > 4)
           break;
       }
-
-      if (k < precision / 4) {
+      // if after a certain number of iterations (called "precision") a sequence number did not exceed 2 (a «bail-out»), it belongs to the set and the corresponding point stays black 
+      if (k <= precision) {  // otherwise, it becomes red, green or blue (depending on the iteration number, at which the criterion was fulfilled)
         vertex_array [i * window.getSize().x + j].position = Vector2f(j, i);
-        Color color (k * 255 / (precision / 4), 0, 0);
-        vertex_array [i * window.getSize().x + j].color = color;
-      }
-      else if (k < precision / 2) {
-        vertex_array [i * window.getSize().x + j].position = Vector2f(j, i);
-        Color color (0, k * 255 / (precision / 2), 0);
-        vertex_array [i * window.getSize().x + j].color = color;
-      }
-      else if (k < precision) {
-        vertex_array [i * window.getSize().x + j].position = Vector2f(j, i);
-        Color color (0, 0, k * 255 / precision);
-        vertex_array [i * window.getSize().x + j].color = color;
+        if (k <= precision / 4)
+          vertex_array [i * window.getSize().x + j].color = Color (k * 255 / (precision / 4), 0, 0);  // shades of red (the closer to the boundary of the set, the brighter)
+        else if (k <= precision / 2)
+          vertex_array [i * window.getSize().x + j].color = Color (0, k * 255 / (precision / 2), 0); // shades of green (likewise)
+        else if (k <= precision)
+          vertex_array [i * window.getSize().x + j].color = Color (0, 0, k * 255 / precision); // shades of blue (likewise)
       }
     }
   }
@@ -50,8 +44,9 @@ int main() {
   window.setFramerateLimit (60);
   VertexArray pointmap (Points, window.getSize().x * window.getSize().y);
 
-  int precision = 100;
-  float zoom = 500.f;
+  int precision = 100;  // the maximum number of iterations for a given construction:
+  // if after it "infinity criterion" has not been reached yet, we conclude that this point belongs to Mandelbrot's set; otherwise - it doesn't belong to the set
+  float zoom = 500.f;  // it is applied so as to illustrate this set on the screen in a full scale
 
   Mandelbrot_set (pointmap, precision, zoom, window);
 
