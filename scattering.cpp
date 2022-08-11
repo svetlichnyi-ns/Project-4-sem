7be8bec74fbebeca7dd24540b71f51123f100256
nikita <svetlichnyi.ns@phystech.edu>
2022-08-11 03:19:18 +0300

    Added comments, made a refactoring, optimized the program by making it faster and more effective

--- scattering.cpp
+++ scattering.cpp
@@ -1,7 +1,6 @@
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
-#include <stdint.h>
 #include <math.h>
 #include <sstream>
 
@@ -10,38 +9,41 @@ using namespace std;
 
 int main() {
   RenderWindow window(VideoMode(1920, 1080), "Scattering of points");
-  window.setFramerateLimit(60);
+  window.setFramerateLimit(40);  // this frame rate provides both optimal perception and rather high computational performance
 
-  RectangleShape square(Vector2f(800.f, 800.f));
+  RectangleShape square(Vector2f(800.f, 800.f));  // creating a square
   square.setOrigin(Vector2f(square.getSize().x / 2, square.getSize().y / 2));
   square.setPosition(Vector2f(window.getSize().x / 2, window.getSize().y / 2));
   square.setOutlineThickness(10.f);
   square.setOutlineColor(Color::Yellow);
+  window.draw(square);
 
-  CircleShape circle;
+  RectangleShape patch(Vector2f(window.getSize().x, 80.f));  // creating a black patch at the top of the screen: without it, previous inscriptions would overlap each other
+  patch.setPosition(Vector2f(0.f, 0.f));
+  patch.setFillColor(Color::Black);
+
+  CircleShape circle;  // creating a circle
   circle.setOrigin(Vector2f(square.getSize().x / 2, square.getSize().y / 2));
   circle.setPosition(Vector2f(window.getSize().x / 2, window.getSize().y / 2));
   circle.setOutlineThickness(2.f);
   circle.setRadius(square.getSize().x / 2 - circle.getOutlineThickness());
   circle.setOutlineColor(Color::Black);
-  circle.setPointCount(10000000);
+  circle.setPointCount(10000000);  // number of triangles, forming a circle, which is close to an "ideal" one
+  window.draw(circle);
 
-  VertexArray points(Points, 10000000);
-
-  vector<CircleShape> vector_of_points;
-  long double iterations = 0.0;
+  int iterations = 0;  // the total number of points, depicted on the screen
 
   Font font;
   font.loadFromFile("fonts/a_Futurica_ExtraBold.ttf");
 
   Text text;
-  text.setFont (font);
-  text.setCharacterSize (50);
-  text.setFillColor (Color::White);
-  text.setPosition (Vector2f(350.f, 15.f));
+  text.setFont(font);
+  text.setCharacterSize(50);
+  text.setFillColor(Color::White);
+  text.setPosition(Vector2f(350.f, 15.f));
 
-  long double blue_points = 0.0;
-
+  int blue_points = 0;  // number of points, depicted inside the circle
+  
   while (window.isOpen()) {
     Event event;
     while (window.pollEvent(event)) {
@@ -49,37 +51,27 @@ int main() {
         window.close();
     }
 
-    window.clear();
-    window.draw(square);
-    window.draw(circle);
-
-    #pragma omp parallel for
-    for (int i = iterations; i < iterations + 10; i++) {
-      points[i].position = Vector2f((float) (window.getSize().x / 2) - square.getSize().x / 2 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / square.getSize().x)),
-                                    (float) (window.getSize().y / 2) - square.getSize().y / 2 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / square.getSize().y)));
-      if (pow(square.getPosition().x - points[i].position.x, 2.f) + pow(square.getPosition().y - points[i].position.y, 2.f) <= pow(circle.getRadius() + circle.getOutlineThickness(), 2.f)) {
-        points[i].color = Color::Blue;
-        blue_points++;
-      }
-      else
-        points[i].color = Color::Red;
-      CircleShape point(2.f, 100000);
-      point.setFillColor(points[i].color);
-      point.setOrigin(point.getRadius(), point.getRadius());
-      point.setPosition(points[i].position.x, points[i].position.y);
-      vector_of_points.push_back(point);
-        
+    CircleShape point(2.f, 100000);  // we actually draw not points, but tiny circles, so that they are seen for us
+    point.setOrigin(point.getRadius(), point.getRadius());
+    point.setPosition(Vector2f((float) (window.getSize().x / 2) - square.getSize().x / 2 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / square.getSize().x)),
+                              (float) (window.getSize().y / 2) - square.getSize().y / 2 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / square.getSize().y))));
+    if (pow(square.getPosition().x - point.getPosition().x, 2.f) + pow(square.getPosition().y - point.getPosition().y, 2.f) <= pow(circle.getRadius() + circle.getOutlineThickness(), 2.f)) {
+      point.setFillColor(Color::Blue);  // if the point occurs inside the circle or on its border, it becomes blue
+      blue_points++;
     }
-    iterations = iterations + 10.0;
+    else
+      point.setFillColor(Color::Red); // if the point occurs outside the circle, it becomes red
+
+    iterations++;
     ostringstream num_of_iterations;
     num_of_iterations << iterations;
     ostringstream pi;
-    pi << (long double) (4 * blue_points / iterations);
+    pi << 4 * (long double) blue_points / iterations; // number pi is calculated via the probability for the point not to occur outside the circle
     text.setString ("Number of iterations: " + num_of_iterations.str() + "; number pi = " + pi.str());
+    window.draw(point);
     window.draw(text);
-    for (int i = 0; i < vector_of_points.size(); i++)
-      window.draw(vector_of_points[i]);
     window.display();
+    window.draw(patch);
   }
   return 0;
 }
