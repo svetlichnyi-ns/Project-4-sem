166c0a473d5b49db2840d88ec5099facad0f7957
nikita <sns_02@mail.ru>
2022-08-20 22:56:20 +0300

    Added a short program, calculating number pi by Viete's formula

---
+++ Viete.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstdint>
+#include <chrono>
+#include <cmath>
+
+int main() {
+  uint64_t N;  // the number of factors that make up the product
+  std::cout << "Enter the number of factors in Viete's formula: ";
+  std::cin >> N;
+  long double* factors = (long double*) malloc (N * sizeof(long double));  // a dynamic array of factors
+  if (factors == NULL) {
+    std::cerr << "Failed to allocate memory via malloc()\n";
+    return -1;
+  }
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();  // start the timer
+  factors[0] = 2.l / sqrtl(2.l);  // initialize the zeroth factor
+  long double pi = 2.l * factors[0];
+  for (uint64_t i = 1; i < N; i++) {
+    factors[i] = 2.l / sqrtl(2.l + 2.l / factors[i - 1]);  // nested radicals
+    pi *= factors[i];
+  }
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();  // stop the timer
+  std::cout << "Number PI, obtained as a product of " << N << " factors, is equal to " << std::setprecision(15) << pi << std::endl;
+  std::cout << "The calculation took " << (std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()) / 1'000'000'000.l << " seconds\n";
+  free(factors);  // free allocated memory
+  return 0;
+}
