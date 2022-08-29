f09c7094b8a33951b84f65b1985bdd6d83e8a76b
nikita <sns_02@mail.ru>
2022-08-29 08:08:46 +0300

    Changed the principle of working with namespaces

--- scattering.cpp
+++ scattering.cpp
@@ -4,68 +4,65 @@
 #include <math.h>
 #include <sstream>
 
-using namespace sf;
-using namespace std;
-
 int main() {
-  RenderWindow window(VideoMode(1920, 1080), "Scattering of points");
+  sf::RenderWindow window(sf::VideoMode(1920, 1080), "Scattering of points");
   window.setFramerateLimit(40);  // this frame rate provides both optimal perception and rather high computational performance
 
-  RectangleShape square(Vector2f(800.f, 800.f));  // creating a square
-  square.setOrigin(Vector2f(square.getSize().x / 2, square.getSize().y / 2));
-  square.setPosition(Vector2f(static_cast<float>(window.getSize().x / 2), static_cast<float>(window.getSize().y / 2)));
+  sf::RectangleShape square(sf::Vector2f(800.f, 800.f));  // creating a square
+  square.setOrigin(sf::Vector2f(square.getSize().x / 2, square.getSize().y / 2));
+  square.setPosition(sf::Vector2f(static_cast<float>(window.getSize().x / 2), static_cast<float>(window.getSize().y / 2)));
   square.setOutlineThickness(10.f);
-  square.setOutlineColor(Color::Yellow);
+  square.setOutlineColor(sf::Color::Yellow);
   window.draw(square);
 
-  RectangleShape patch(Vector2f(static_cast<float>(window.getSize().x), 80.f));  // creating a black patch at the top of the screen: without it, previous inscriptions would overlap each other
-  patch.setPosition(Vector2f(0.f, 0.f));
-  patch.setFillColor(Color::Black);
+  sf::RectangleShape patch(sf::Vector2f(static_cast<float>(window.getSize().x), 80.f));  // creating a black patch at the top of the screen: without it, previous inscriptions would overlap each other
+  patch.setPosition(sf::Vector2f(0.f, 0.f));
+  patch.setFillColor(sf::Color::Black);
 
-  CircleShape circle;  // creating a circle
-  circle.setOrigin(Vector2f(square.getSize().x / 2, square.getSize().y / 2));
-  circle.setPosition(Vector2f(static_cast<float>(window.getSize().x / 2), static_cast<float>(window.getSize().y / 2)));
+  sf::CircleShape circle;  // creating a circle
+  circle.setOrigin(sf::Vector2f(square.getSize().x / 2, square.getSize().y / 2));
+  circle.setPosition(sf::Vector2f(static_cast<float>(window.getSize().x / 2), static_cast<float>(window.getSize().y / 2)));
   circle.setOutlineThickness(2.f);
   circle.setRadius(square.getSize().x / 2 - circle.getOutlineThickness());
-  circle.setOutlineColor(Color::Black);
+  circle.setOutlineColor(sf::Color::Black);
   circle.setPointCount(10000000);  // number of triangles, forming a circle, which is close to an "ideal" one
   window.draw(circle);
 
   int iterations = 0;  // the total number of points, depicted on the screen
 
-  Font font;
+  sf::Font font;
   font.loadFromFile("fonts/a_Futurica_ExtraBold.ttf");
 
-  Text text;
+  sf::Text text;
   text.setFont(font);
   text.setCharacterSize(50);
-  text.setFillColor(Color::White);
-  text.setPosition(Vector2f(350.f, 15.f));
+  text.setFillColor(sf::Color::White);
+  text.setPosition(sf::Vector2f(350.f, 15.f));
 
   int blue_points = 0;  // number of points, depicted inside the circle
   
   while (window.isOpen()) {
-    Event event;
+    sf::Event event;
     while (window.pollEvent(event)) {
-      if (event.type == Event::Closed)
+      if (event.type == sf::Event::Closed)
         window.close();
     }
 
-    CircleShape point(2.f, 100000);  // we actually draw not points, but tiny circles, so that they are seen for us
+    sf::CircleShape point(2.f, 100000);  // we actually draw not points, but tiny circles, so that they are seen for us
     point.setOrigin(point.getRadius(), point.getRadius());
-    point.setPosition(Vector2f((float) (window.getSize().x / 2) - square.getSize().x / 2 + square.getSize().x * static_cast<float>(rand()) / static_cast<float>(RAND_MAX),
+    point.setPosition(sf::Vector2f((float) (window.getSize().x / 2) - square.getSize().x / 2 + square.getSize().x * static_cast<float>(rand()) / static_cast<float>(RAND_MAX),
                               (float) (window.getSize().y / 2) - square.getSize().y / 2 + square.getSize().y * static_cast<float>(rand()) / static_cast<float>(RAND_MAX)));
     if (pow(square.getPosition().x - point.getPosition().x, 2.f) + pow(square.getPosition().y - point.getPosition().y, 2.f) <= pow(circle.getRadius() + circle.getOutlineThickness(), 2.f)) {
-      point.setFillColor(Color::Blue);  // if the point occurs inside the circle or on its border, it becomes blue
+      point.setFillColor(sf::Color::Blue);  // if the point occurs inside the circle or on its border, it becomes blue
       blue_points++;
     }
     else
-      point.setFillColor(Color::Red); // if the point occurs outside the circle, it becomes red
+      point.setFillColor(sf::Color::Red); // if the point occurs outside the circle, it becomes red
 
     iterations++;
-    ostringstream num_of_iterations;
+    std::ostringstream num_of_iterations;
     num_of_iterations << iterations;
-    ostringstream pi;
+    std::ostringstream pi;
     pi << 4 * (long double) blue_points / iterations; // number pi is calculated via the probability for the point not to occur outside the circle
     text.setString ("Number of iterations: " + num_of_iterations.str() + "; number pi = " + pi.str());
     window.draw(point);
