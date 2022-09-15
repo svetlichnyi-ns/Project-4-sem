cacc53ca0a2ad7bc63ed4f76df38e65267458c93
nikita <sns_02@mail.ru>
2022-09-15 12:01:09 +0300

    Left only the main function in the header file

--- series.h
+++ series.h
@@ -1,9 +1,7 @@
 #ifndef SERIES_H
 #define SERIES_H
 
-// a function that conducts calculations and sums up their intermediate and main results
-int calculation_of_pi_via_series (int way, int N);
-// a small function that launches all supported experiments
+// a function that conducts calculations, sums up their intermediate and main results and launches all supported experiments
 int series();
 
 #endif
