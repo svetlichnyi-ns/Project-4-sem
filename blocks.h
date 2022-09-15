a3de490b9476e740d4756f89348a2f2868228051
nikita <sns_02@mail.ru>
2022-09-15 11:52:15 +0300

    Changed the header file

--- blocks.h
+++ blocks.h
@@ -1,11 +1,6 @@
 #ifndef BLOCKS_H
 #define BLOCKS_H
-#include <SFML/System.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
 
-// this function calculates blocks' velocities after their absolutely elastic collision
-sf::Vector2f elastic_collision (sf::Vector2f initial_velocities, sf::Vector2i masses);
 // a main function that is responsible for graphical interpretation, interaction with user, counting PI and conclusion of results
 int blocks();
 
