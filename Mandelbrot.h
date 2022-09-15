a6418d683720f52ce483fbc46f40f7833eb9c9ce
nikita <sns_02@mail.ru>
2022-09-15 11:49:06 +0300

    Got rid of unnecessary details in the header file

--- Mandelbrot.h
+++ Mandelbrot.h
@@ -1,19 +1,7 @@
 #ifndef MANDELBROT_H
 #define MANDELBROT_H
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Graphics.hpp>
 
-class complex_number {
-  public:
-    long double real, imaginary;
-};
-
-// this function is responsible for drawing Mandelbrot's set
-void Mandelbrot_set (sf::VertexArray& vertex_array, int precision, long double zoom, sf::RenderWindow& window);
-// this function implements numerical algorithm of PI's computation
-void Mandelbrot_iterations(long double eps);
-// this function unites opportunities of two previous ones and launches experiments
+// this function draws Mandelbrot's set, implements numerical algorithm of PI's computation and launches experiments
 int Mandelbrot();
 
 #endif
