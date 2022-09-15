7ff5349f38396060c11adf1e69a9d50427fc45bb
nikita <sns_02@mail.ru>
2022-09-15 12:53:18 +0300

    Transformed the algorithm in such a way that it does not need vectors or dynamic arrays anymore

--- Viete.cpp
+++ Viete.cpp
@@ -4,33 +4,30 @@
 #include <cstdint>
 #include <chrono>
 #include <cmath>
-#include <cstdlib>
 #include "Viete.h"
 
 int Viete() {
-  uint64_t N;  // the number of factors that make up the product
-  std::cout << "Enter the number of factors in Viete's formula: ";
+  uint64_t N;  // the number of factors (on the right side of the formula) that make up the product
+  std::cout << "Enter the number of factors in Viete's formula (at least, two): ";
   std::cin >> N;
-  if (N <= 0) {
-    std::cerr << "The number of factors must be positive.\n";
-    return -1;
-  }
-  long double* factors = (long double*) malloc (N * sizeof(long double));  // a dynamic array of factors
-  if (factors == NULL) {
-    std::cerr << "Failed to allocate memory via malloc()\n";
+  if (N <= 1) {
+    std::cerr << "The number of factors must be not less than two.\n";
     return -1;
   }
   std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();  // start the timer
-  factors[0] = 2.l / sqrtl(2.l);  // initialize the zeroth factor
-  long double pi = 2.l * factors[0];
-  for (uint64_t i = 1; i < N; i++) {
-    factors[i] = 2.l / sqrtl(2.l + 2.l / factors[i - 1]);  // nested radicals
-    pi *= factors[i];
+  long double previous_factor = 2.l / sqrtl(2.l);
+  long double pi = 2.l * previous_factor;
+  long double next_factor = 2.l / sqrtl(2.l + 2.l / previous_factor);
+  pi *= next_factor;
+  // now we have already calculated the number PI, based on two factors
+  for (uint64_t i = 2; i < N; i++) {
+    previous_factor = next_factor;
+    next_factor = 2.l / sqrtl(2.l + 2.l / previous_factor);
+    pi *= next_factor;
   }
   std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();  // stop the timer
   std::cout << "Number PI, obtained as a product of " << N << " factors, is equal to: " << std::setprecision(15) << pi << ";\n";
   std::cout << "The calculation error is: " << fabsl(pi - M_PIl) << ";\n";
   std::cout << "The calculation took " << (std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()) / 1'000'000'000.l << " seconds.\n\n";
-  free(factors);  // free allocated memory
   return 0;
 }
